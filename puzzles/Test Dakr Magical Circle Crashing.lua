204d38d66ebc5e95ebef9a16728cf27ac851f1a5
Naim <naimsantos45@gmail.com>
2019-07-10 17:26:36 -0300

    Create Test Dakr Magical Circle Crashing.lua

---
+++ puzzles/Test Dakr Magical Circle Crashing.lua
@@ -0,0 +1,16 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--Main Deck
+Debug.AddCard(46986414,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(88619463,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(12580477,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(17092736,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(83266092,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+--Hand
+Debug.AddCard(47222536,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(96677818,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
