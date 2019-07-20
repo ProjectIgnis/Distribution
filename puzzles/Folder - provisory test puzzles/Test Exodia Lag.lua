7f0cd4bebdf744e1bb82e07ccdf40d213e330361
Naim <naimsantos45@gmail.com>
2019-07-20 10:36:32 -0300

    Renaming puzzles

---
+++ puzzles/Folder - provisory test puzzles/Test Exodia Lag.lua
@@ -0,0 +1,16 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--Hand
+Debug.AddCard(70903634,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(7902349,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(8124921,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(33396948,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+--GY
+Debug.AddCard(58604027,0,0,LOCATION_GRAVE,0,POS_FACEUP)
+Debug.AddCard(44519536,0,0,LOCATION_GRAVE,0,POS_FACEUP)
+--Monster Zones
+Debug.AddCard(58604027,0,0,LOCATION_MZONE,1,1,true)
+Debug.ReloadFieldEnd()
