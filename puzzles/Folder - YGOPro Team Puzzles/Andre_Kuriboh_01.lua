622a06b7509261b0be7ebfd485f9ee4a9911542c
Naim <naimsantos45@gmail.com>
2019-08-05 09:30:04 -0300

    Update Andre_Kuriboh_01.lua

--- puzzles/Folder - YGOPro Team Puzzles/Andre_Kuriboh_01.lua
+++ puzzles/Folder - YGOPro Team Puzzles/Andre_Kuriboh_01.lua
@@ -1,4 +1,9 @@
---joke puzzle
+--Kuriboh Madness v1
+--Designed and implemented by Andre
+
+--[[message
+Are Kuribohs a joke?
+Be careful while choosing an attacker.]]
 function aux.trol(e,tp)
 	local c=e:GetHandler()
 	local a=Duel.GetAttacker()
@@ -17,7 +22,7 @@ function aux.trol(e,tp)
 	end
 end
 Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI)
-Debug.SetAIName("evil guy")
+Debug.SetAIName("Evil Guy")
 Debug.SetPlayerInfo(0,1,0,0)
 Debug.SetPlayerInfo(1,1000,0,0)
 --you card
@@ -27,8 +32,8 @@ Debug.AddCard(23995346,0,0,LOCATION_MZONE,1,POS_FACEUP_ATTACK)
 Debug.AddCard(3606728,1,1,LOCATION_MZONE,0,POS_FACEUP_ATTACK)
 Debug.ReloadFieldEnd()
 local e1=Effect.CreateEffect(c)
-e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-e1:SetCountLimit(1)
-e1:SetOperation(aux.trol)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCountLimit(1)
+	e1:SetOperation(aux.trol)
 Duel.RegisterEffect(e1,0)
