7ca92e53a3dbc99877baa51fd1235abe7068ae3f
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2019-08-26 10:16:18 -0400

    Add autoassembler script

---
+++ assemble.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Expected directory setup in parent folder: edopro-config, edopro-cdb, edopro-media, edopro-script
+# edopro-bin, edopro-windows, edopro-osx, edopro-linux with appropriate upstream remote ONLY
+# Edopro_1034 with correct username and email setup without extraneous files
+# Pass the appropriate authentication in as $1
+
+set -euxo pipefail
+
+cd $(dirname $0)/..
+NEEDS_UPDATE=0
+TARGETS="edopro-config edopro-cdb edopro-media edopro-script edopro-bin edopro-windows edopro-osx edopro-linux"
+SIMPLY_COPY="edopro-config edopro-cdb edopro-media edopro-bin"
+for REPO in $TARGETS; do
+    cd $REPO
+    git remote update
+    LOCAL=$(git rev-parse HEAD)
+    UPSTREAM=$(git rev-parse @{u})
+    if [[ $LOCAL != $UPSTREAM ]]; then
+        git reset --hard @{u}
+        NEEDS_UPDATE=1
+    fi
+    cd ..
+done
+
+if [[ NEEDS_UPDATE == 1 ]]; then
+    cd Edopro_1034
+    git remote update
+    git reset --hard @{u}
+    for REPO in $SIMPLY_COPY; do
+        rsync -ar ../$REPO/* .
+    done
+    rm assemble.sh
+    cp ../edopro-windows/ygoprodll.exe edopro.exe
+    cp ../edopro-windows/ocgcore.dll .
+    cp ../edopro-linux/ygoprodll edopro
+    cp ../edopro-linux/libocgcore.so ../edopro-linux/libgit2.so.26 .
+    rm -rf edopro.app
+    cp -r ../edopro-osx/ygoprodll.app edopro.app
+    mv edopro.app/Contents/MacOS/ygoprodll edopro.app/Contents/MacOS/edopro
+    cp ../edopro-osx/libocgcore.dylib .
+    rsync -ar --exclude=.git ../edopro-script/ script
+    rm -rf .git
+    git init
+    git add -A .
+    git commit -m "EDOPro Automatic Assembly"
+    git push -f https://$1@github.com/YgoproStaff/Edopro_1034.git master:master
+fi
