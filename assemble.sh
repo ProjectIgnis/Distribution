aaa1d871cb87369d1c11fa8f3d056c89834acd28
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2019-08-28 16:45:47 -0400

    Update assemble for shared-only build config

--- assemble.sh
+++ assemble.sh
@@ -7,9 +7,11 @@
 set -euxo pipefail
 
 cd $(dirname $0)/..
+
 NEEDS_UPDATE=0
-TARGETS="edopro-config edopro-cdb edopro-media edopro-script edopro-bin edopro-windows edopro-osx edopro-linux"
+TARGETS="edopro-config edopro-cdb edopro-media edopro-script edopro-bin edopro-windows edopro-osx edopro-linux core-windows core-osx core-linux"
 SIMPLY_COPY="edopro-config edopro-cdb edopro-media edopro-bin"
+
 for REPO in $TARGETS; do
     cd $REPO
     git remote update
@@ -27,18 +29,26 @@ if [[ $NEEDS_UPDATE == 1 ]]; then
     git remote update
     git reset --hard @{u}
     git reset --soft HEAD^
+
+    rm edopro
+    rm edopro.exe
+    rm -rf edopro.app
     for REPO in $SIMPLY_COPY; do
         rsync -ar ../$REPO/* .
     done
     rm assemble.sh
-    cp ../edopro-windows/ygoprodll.exe edopro.exe
-    cp ../edopro-windows/ocgcore.dll .
+
     cp ../edopro-linux/ygoprodll edopro
-    rm -rf edopro.app
+    cp ../edopro-windows/ygoprodll.exe edopro.exe
     cp -r ../edopro-osx/ygoprodll.app edopro.app
     mv edopro.app/Contents/MacOS/ygoprodll edopro.app/Contents/MacOS/edopro
-    cp ../edopro-osx/libocgcore.dylib .
+
+    cp ../core-osx/libocgcore.dylib .
+    cp ../core-linux/libocgcore.so .
+    cp ../core-windows/ocgcore.dll .
+
     rsync -ar --exclude=.git ../edopro-script/ script
+
     git add -A .
     git commit -m "EDOPro Automatic Assembly"
     git push -f origin master:master
