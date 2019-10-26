70e458746c6bae35a1cd692229340a8b58fd0752
Edoardo Lolletti <edoardo762@gmail.com>
2019-10-26 12:31:46 +0200

    Update Test Grinder_Gnomem.lua
    Added solemn judgment interaction

--- puzzles/Folder - Puzzles to test Edopro/Test Grinder_Gnomem.lua
+++ puzzles/Folder - Puzzles to test Edopro/Test Grinder_Gnomem.lua
@@ -3,10 +3,12 @@ Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
 Debug.SetPlayerInfo(0,8000,0,0)
 Debug.SetPlayerInfo(1,8000,0,0)
 --[[message
-	If Grinder Golem Golem is Special Summoned and Summons the tokens while the player controls, Gnomaterial should be be able to be activated
+	If Grinder Golem Golem is Special Summoned and Summons the tokens while the player controls, Gnomaterial should NOT be be able to be activated.
+	Also if solemn jusgment is activated, the summoned tokens too should be destroyed, as the summon happens at the same time.
 ]]
 --Hand
 Debug.AddCard(58655504,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(41420027,0,0,LOCATION_SZONE,0,POS_FACEDOWN)
 --Hand
 Debug.AddCard(75732622,1,1,LOCATION_HAND,0,POS_FACEDOWN)
 Debug.ReloadFieldEnd()
