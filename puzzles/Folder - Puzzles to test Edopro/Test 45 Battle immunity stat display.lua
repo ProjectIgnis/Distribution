029f8c6a8aaeb1f14ef1325471eee39ca628da0d
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2019-10-28 13:45:57 -0400

    Add display inconsistency puzzle

---
+++ puzzles/Folder - Puzzles to test Edopro/Test 45 Battle immunity stat display.lua
@@ -0,0 +1,23 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+Debug.AddCard(18161786,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(18161786,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+--Mystic Plasma Zone
+Debug.AddCard(18161786,0,0,LOCATION_FZONE,0,10)
+--Dark Spirit Art - Greed
+Debug.AddCard(38167722,0,0,LOCATION_SZONE,2,10)
+--DNA Transplant
+Debug.AddCard(56769674,0,0,LOCATION_SZONE,3,10)
+--Mystik Wok
+Debug.AddCard(80161395,0,0,LOCATION_SZONE,4,10)
+--Star Eater
+Debug.AddCard(41517789,0,0,LOCATION_MZONE,5,1,true)
+--Shrink
+Debug.AddCard(55713623,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+--Ritual Beast Ulti-Apelio
+Debug.AddCard(86274272,0,0,LOCATION_MZONE,2,1,true)
+--Mirror Force
+Debug.AddCard(44095762,1,1,LOCATION_SZONE,3,10)
+Debug.ReloadFieldEnd()
