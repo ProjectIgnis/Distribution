3330356e084996250bed0c9177b015bf6739607b
NaimSantos <naimsantos45@gmail.com>
2019-10-31 09:51:52 -0300

    Updated Test puzzle: Marincess Loop

--- puzzles/Folder - Tests for known bugs in Percy/Test 64 - Crystal Heart Infinite Loop.lua
+++ puzzles/Folder - Tests for known bugs in Percy/Test 64 - Crystal Heart Infinite Loop.lua
@@ -2,11 +2,14 @@
 Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
 Debug.SetPlayerInfo(0,8000,0,0)
 Debug.SetPlayerInfo(1,8000,0,0)
+--[[message
+An infinite loop of Crystal Heart vs Graydle monsters
+]]
 
 --Monster Zones
 Debug.AddCard(29834183,0,0,LOCATION_MZONE,1,1,true)
 --Monster Zones
-Debug.AddCard(101010040,1,1,LOCATION_MZONE,6,1,true)
+Debug.AddCard(67712104,1,1,LOCATION_MZONE,6,1,true)
 Debug.AddCard(79130389,1,1,LOCATION_MZONE,2,1,true)
 Debug.ReloadFieldEnd()
 aux.BeginPuzzle()
