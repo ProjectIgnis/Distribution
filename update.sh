af9658a40c64ed84b75e09bf3c8a15fa2034eb1e
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2019-11-29 14:16:43 -0500

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+git fetch && git reset --hard @{u}
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    git checkout origin/windbot-linux -- ygoprodll
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    git checkout origin/windbot-osx -- ygoprodll.app
+    sed -i "" 's/use_vsync = 1/use_vsync = 0/g' system.conf
+else
+    git checkout origin/windbot-windows -- ygoprodll.exe
+fi
+
