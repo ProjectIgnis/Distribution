d678daea1f6fc29b050d5c7708b16adabccd50e4
kevinlul <6320810+kevinlul@users.noreply.github.com>
2020-02-17 16:35:28 -0500

    Add puzzle for Fluorohydride/ygopro-core#289

---
+++ puzzles/Folder - Tests for known bugs in Percy/Flu 289 - Superheavy Borrelsword.lua
@@ -0,0 +1,10 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000800,0,0)
+
+--Monster Zones
+Debug.AddCard(50065971,0,0,LOCATION_MZONE,2,1,true)
+--Monster Zones
+Debug.AddCard(85289965,1,1,LOCATION_MZONE,2,1,true)
+Debug.ReloadFieldEnd()
